9861a9a42e6adbdb73997e1b93de918422cb8d14
alejo-desktop <alejoassain00@gmail.com>
2023-02-25 08:52:03 -0300

    initial commit

---
+++ src/main/java/com/alejoassain/portfolioargprogbe/PortfolioArgprogBeApplication.java
@@ -0,0 +1,13 @@
+package com.alejoassain.portfolioargprogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioArgprogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioArgprogBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alejoassain/portfolioargprogbe/user/Role.java
@@ -0,0 +1,6 @@
+package com.alejoassain.portfolioargprogbe.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
